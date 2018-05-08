da1c62ad7abb24d8ed2a6b4b975f9c206a580b92
adam-conway <adam.n.conway@gmail.com>
2018-05-07 19:48:11 -0600

    Added oauth as well as figaro

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,8 @@ class User < ApplicationRecord
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid                = auth_info.uid
-      binding.pry
-      new_user.name               = auth_info.extra.raw_info.name
-      new_user.screen_name        = auth_info.extra.raw_info.screen_name
-      new_user.oauth_token        = auth_info.credentials.token
-      new_user.oauth_token_secret = auth_info.credentials.secret
+      new_user.email              = auth_info.info.email
+      new_user.token              = auth_info.credentials.token
     end
   end
 end
