6b19f6f252d5bf9135f68b53c133ffe28a560ae3
adam-conway <adam.n.conway@gmail.com>
2018-05-10 11:36:39 -0600

    Added statquery test

---
+++ spec/services/stat_query_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe StatQuery do
+
+  context 'initialize' do
+    it 'exists with correct token' do
+      user = create(:user)
+      subject = StatQuery.new(user)
+      expect(subject).to be_an StatQuery
+      expect(subject.token).to eq(ENV['strava_my_token'])
+    end
+  end
+
+  context 'instance methods' do
+    context '#stats' do
+      it 'returns stat information' do
+        VCR.use_cassette("getting-stat-information") do
+          user = create(:user)
+          subject = StatQuery.new(user)
+          expect(subject.stat_information).to be_a Stat
+        end
+      end
+    end
+  end
+end
