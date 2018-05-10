7002e8dfdaae86e52e9829be19ba1bc0b448fe16
adam-conway <adam.n.conway@gmail.com>
2018-05-10 15:56:11 -0600

    Added all stats to profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,6 @@
 class ProfileController < ApplicationController
   def index
     @profile = ProfileQuery.new(current_user).profile_information
+    @stats = StatQuery.new(current_user).stat_information
   end
 end
